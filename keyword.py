62c8fd980e88e2c435d20ba4b8612fab632af0c4
Krithikasri <krithisri412@gmail.com>
2019-08-03 20:51:56 +0530

    Create keyword.py

---
+++ keyword.py
@@ -0,0 +1,6 @@
+keyword={"if","else","goto","range","break","continue","elif","in","return","for","in"}
+i=input()
+if i in keyword:
+        print("Given string is keyword")
+else:
+    print("Not")
